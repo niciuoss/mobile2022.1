37ccd4037ab7f22999d6ca7ad8d999a7b8c680e9
Vinicius Sales <viniciusamorimsales@gmail.com>
2022-06-25 03:49:31 -0300

    Delete

--- ProjetoFinal/app/src/main/java/com/example/projetofiinal/transactions/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.example.projetofiinal.transactions;
-
-public class Constants {
-    public static int REQUEST_ADD_IMOVEL = 10;
-    public static int REQUEST_EDIT = 11;
-    public static int RES_ADD = 20;
-    public static int RES_CANCEL = 21;
-}
