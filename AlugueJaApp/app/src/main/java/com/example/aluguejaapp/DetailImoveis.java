18f96eb2da991297b8234843ad3fc9142b157feb
WeslliVs <wesllivs@gmail.com>
2022-07-12 20:43:34 -0300

    Mapa e Busca Finalizados

--- AlugueJaApp/app/src/main/java/com/example/aluguejaapp/DetailImoveis.java
+++ AlugueJaApp/app/src/main/java/com/example/aluguejaapp/DetailImoveis.java
@@ -1,10 +1,13 @@
 package com.example.aluguejaapp;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
 
 import android.app.Activity;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -14,16 +17,52 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+
 public class DetailImoveis extends FragmentActivity implements OnMapReadyCallback {
 
     GoogleMap map;
+    SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_imoveis);
 
+        searchView = findViewById(R.id.idSearchView);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+
+                String location = searchView.getQuery().toString();
+                List<Address> addressList = null;
+
+                if ((location != null) || location.equals("")){
+                    Geocoder geocoder = new Geocoder(DetailImoveis.this);
+                    try {
+                        addressList = geocoder.getFromLocationName(location, 1);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    Address address = addressList.get(0);
+
+                    LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+                    map.addMarker(new MarkerOptions().position(latLng).title(location));
+                    map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17f));
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
         mapFragment.getMapAsync(this);
     }
 
@@ -32,7 +71,6 @@ public class DetailImoveis extends FragmentActivity implements OnMapReadyCallbac
         map = googleMap;
 
         LatLng Ceara = new LatLng(-5.489161114975658, -39.31813813058585);
-        map.addMarker(new MarkerOptions().position(Ceara).title("Ceara"));
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(Ceara, 8f));
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(Ceara, 7f));
     }
 }
